5f81df8f414a29ca1d270ca89d86e76a0064b623
Rvmzez <146724340+Rvmzez@users.noreply.github.com>
2024-01-29 18:31:54 +0900

    проект5

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,31 @@
-// @todo: Темплейт карточки
+const placesList = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
+const addButton = document.querySelector('.profile__add-button');
 
-// @todo: DOM узлы
+const createCard = initialCards.map(function (item) {
+    return {
+        name: item.name,
+        link: item.link
+    }
+});
 
-// @todo: Функция создания карточки
+function render () {
+    initialCards.forEach(renderCard)
+};
 
-// @todo: Функция удаления карточки
+function renderCard ({name, link}) {
+    const cardElement = cardTemplate.cloneNode(true);
 
-// @todo: Вывести карточки на страницу
+    cardElement.querySelector('.card__image').src = link;
+    cardElement.querySelector('.card__title').textContent = name;
+
+    placesList.append(cardElement)
+};
+
+render();
+
+document.querySelector('.page__content').onclick = function (delBtn) {
+    if(delBtn.target.className != 'card__delete-button') return
+    const crdTmpl = delBtn.target.closest('.places__item')
+       crdTmpl.remove()
+    }
